6ebdc285e286a881e7f05af220802483b1c7b75d
ityoup <lizarraga.juan.1gv@gmail.com>
2023-05-05 10:43:05 -0600

    update

---
+++ src/controllers/anuario.controller.js
@@ -0,0 +1,48 @@
+import { con } from "../controllers/db.js";
+
+export const subirImagen = async (req, res) => {
+    const [resultados] = await con.query("Select * from imagenes");
+    res.render('subirImagen', { resultados: resultados })
+}
+
+export const anuario = async (req, res) => {
+
+    //Se busca dentro de la base de datos que valor 
+
+    let pruebaaaa = ["W"]
+    let [chido] = await con.query("Select MAX(id) AS Ultimo from imagenes")
+    let imagenFor = chido[0].Ultimo;
+    let [busquedaImg] = await con.query(`SELECT imagenes from imagenes where id=${imagenFor}`);
+    // catch para saber si la imagen tiene 
+
+
+
+    try {
+        new ExifImage({ image: `public/${busquedaImg[0].imagenes}` }, async function (error, exifData) {
+
+            const [resultados] = await con.query("Select * from imagenes");
+            let fecha = new Date();
+            var date = fecha.getFullYear() + '-' + (fecha.getMonth() + 1) + '-' + fecha.getDate();
+
+            let fechaCreacionImagen;
+            try {
+                fechaCreacionImagen = exifData.exif.DateTimeOriginal;
+                console.log(fechaCreacionImagen);
+
+            } catch (error) {
+
+                console.log(error)
+                // res.render('anuario', { resultados: resultados, fechaExif: `${fechaCreacionImagen}`,fechaHoy: date })
+            }
+
+            res.render('anuario', { resultados: resultados, fechaExif: `${fechaCreacionImagen}`, fechaHoy: date })
+
+        });
+    } catch (error) {
+        console.log('Error: ' + error.message);
+    }
+
+
+
+
+}
--- src/routes/rutas.routes.js
+++ src/routes/rutas.routes.js
@@ -1,11 +1,30 @@
 import { Router } from "express";
+import { con } from "../controllers/db.js";
 import { raiz } from "../controllers/main.controller.js";
+import { subirImagen, anuario } from "../controllers/anuario.controller.js";
+import multer from "multer";
+const subida = multer({ dest: 'public/' })
 const rutas = Router();
 
 //Ruta Veterinaria
 rutas.get('/', raiz);
 
+//Ruta para subir imagenes al anuario
+rutas.get('/subirImagen', subirImagen);
 
+//Post para la subida de archivos junto con multer
+rutas.post('/upload', subida.single('subirArchivo'), function (req, res, next) {
+
+   res.redirect('/subirImagen')
+   const numFile = req.file.filename;
+   console.log(numFile)
+
+   try {
+      con.query(`INSERT INTO imagenes (imagenes) values ('${numFile}')`)
+   } catch (error) {
+      console.log('no se pudo insertar el valor');
+   }
+})
 
 
 
