8b26bc27f0855f14fc4fb427ba48bc068ff31e4b
ityoup <lizarraga.juan.1gv@gmail.com>
2023-05-07 13:49:31 -0600

    update

--- src/controllers/db.js
+++ src/controllers/db.js
@@ -2,7 +2,7 @@ import { createPool } from "mysql2/promise";
 
 
 export const con = createPool({
-    host: '172.17.0.2',
+    host: '172.17.0.3',
     user: 'root',
     password: 'WILDones123',
     port: '3306',
