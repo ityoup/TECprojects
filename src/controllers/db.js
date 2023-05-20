bb9a51a01c232cc16cdc200b8314195b2b46359d
ityoup <lizarraga.juan.1gv@gmail.com>
2023-05-20 09:22:03 -0600

    update de destino de fotos

--- src/controllers/db.js
+++ src/controllers/db.js
@@ -2,7 +2,7 @@ import { createPool } from "mysql2/promise";
 
 
 export const con = createPool({
-    host: '172.17.0.6',
+    host: '172.17.0.2',
     user: 'root',
     password: 'WILDones123',
     port: '3306',
