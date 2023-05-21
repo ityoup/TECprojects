4a9564c3553ea15d960af0c5e41da6522ff3fa88
ityoup <lizarraga.juan.1gv@gmail.com>
2023-05-20 23:35:03 -0600

    update de register

--- src/routes/rutas.routes.js
+++ src/routes/rutas.routes.js
@@ -63,7 +63,7 @@ rutas.post('/acti', pfp.single('pfp'), async function (req, res, next){
           
          con.query(`UPDATE infoAlumnos set semestre='${semestre}', imagen = '${imagen}', materiasFav='${materiaFav}' ,maestrosFav = '${maestroFav}', tecUni = '${tecUni}', ciudad = '${ciudad}', email = '${email}', numTel = ${numTel}, cumple = '${birthday}' where idUser = '${idUser}'`)
          setTimeout(() => {
-            res.redirect('/hubAlumnos')
+            res.redirect('/hubAlumno')
          }, 1000);
         } catch (error) {
          
